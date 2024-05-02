85033b4c9a7f3a307bb79f10930d12a0e4a41ab8
Shubham0202 <shubham826852@gmail.com>
2024-05-02 20:41:22 +0530

    ./

--- my-react-basic-projects/src/Projects/ShowCaseWebsite.jsx
+++ my-react-basic-projects/src/Projects/ShowCaseWebsite.jsx
@@ -4,6 +4,7 @@ import Hero from '../components/Header/Hero'
 import Card from '../components/Card'
 import Image1 from '../assets/download.jpeg'
 import CurrencyConverterImage from '../assets/currencyConverter.jpg'
+import catHolder from '../assets/catHolder.jpeg'
 const ShowCaseWebsite = () => {
   
   return (
@@ -15,6 +16,7 @@ const ShowCaseWebsite = () => {
         <div className="grid place-items-center sm:grid-cols-3 lg:grid-cols-4">
         <Card img={Image1} projectName={"Password Generator"} redirect={"passGen"} className={"sm:mt-20 mt-10"}/>
         <Card img={CurrencyConverterImage} projectName={"Real Time Currency Converter Using API"} redirect={"CurrencyConverter"} className={"mt-10 sm:mt-0 sm:mb-10"}/>
+        <Card img={catHolder} projectName={"Chat App"} redirect={"CurrencyConverter"} className={"mt-10 sm:mt-20"}/>
 
         </div>
     </main>
--- my-react-basic-projects/src/components/Card.jsx
+++ my-react-basic-projects/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({img,projectName,className,redirect}) => {
   return (
     <div className={`${className} group overflow-hidden relative rounded-lg shadow bg-[url(assets/test.jpeg)] bg-center bg-no-repeat w-60 h-80`}>
         <img src={img} alt="" className='absolute w-full h-full rounded-lg shadow shadow-orange-200'/>
-        <h3 className="group-hover:opacity-0 group-hover:translate-y-10 transition duration-500 absolute bottom-0 px-1.5 text-center line-clamp-2 bg-dull-black backdrop-blur-sm rounded-lg text-lg font-bold  text-white">{projectName}</h3>
+        <h3 className="group-hover:opacity-0 group-hover:translate-y-10 transition duration-500 absolute min-h-14 w-full bottom-0 px-1.5 text-center line-clamp-2 bg-dull-black backdrop-blur-sm rounded-lg text-lg font-bold  text-white">{projectName}</h3>
 
         <div id="expand" className='opacity-0 translate-y-96 group-hover:opacity-100 group-hover:translate-y-0 transition duration-500 absolute w-full h-full grid place-items-center bg-dull-black shadow shadow-primary-green rounded-lg'>
           <Link to={redirect} className='capitalize px-6 py-2 rounded-lg bg-black text-white shadow shadow-white hover:shadow-primary-green duration-500 font-semibold '>See Project <LuArrowUpRightFromCircle className='inline text-primary-green'/></Link>
